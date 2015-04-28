127af008ac5fa36282928517f06633e98086fb60
Thomas Bloss <runredfox@gmail.com>
2015-04-28 14:48:58 -0600

    Step 2 Completed

--- Score Keeper/ScoreViewController.m
+++ Score Keeper/ScoreViewController.m
@@ -8,6 +8,19 @@
 
 #import "ScoreViewController.h"
 
+@interface ScoreViewController ()
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@end
+
+
 @implementation ScoreViewController
+-(void)viewDidLoad{
+ 
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [self.view addSubview:self.scrollView];
+    
+}
 
 @end
