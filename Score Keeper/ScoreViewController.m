2867097b85b87eae60007b461cfbfa7edb16d943
Thomas Bloss <runredfox@gmail.com>
2015-04-28 16:39:26 -0600

    Stepper counts

--- Score Keeper/ScoreViewController.m
+++ Score Keeper/ScoreViewController.m
@@ -11,19 +11,22 @@
 @interface ScoreViewController ()
 
 @property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) NSMutableArray *scoreLabels;
 
 @end
 
 
 @implementation ScoreViewController
 -(void)viewDidLoad{
+    
+    self.scoreLabels = [NSMutableArray new];
  
     self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [self.view addSubview:self.scrollView];
     self.title = @"Score Keeper";
     
-    [self  addScoreView:1];
-    
+    [self  addScoreView:0];
+   
 }
 
 -(void)addScoreView:(int)index{
@@ -40,19 +43,38 @@
     UILabel *scoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(105 , 10 , 70, 30)];
     scoreLabel.backgroundColor = [UIColor whiteColor];
     scoreLabel.layer.borderWidth = 1.0;
+    scoreLabel.text = @"0";
     scoreLabel.layer.borderColor = [UIColor cyanColor].CGColor;
+    
+    [self.scoreLabels addObject:scoreLabel];
     [view addSubview:scoreLabel];
+    
     
     UIStepper *stepper = [[UIStepper alloc] initWithFrame:CGRectMake(200, 10, 70, 30)];
     stepper.maximumValue = 100;
     stepper.minimumValue = -10;
+    stepper.tag = index;
     [view addSubview:stepper];
+    
+    [stepper addTarget:(self) action:@selector(stepperTapped:) forControlEvents:UIControlEventValueChanged];
     
     
     [self.scrollView addSubview:view];
     
+
+}
+
+-(void)stepperTapped:(id)sender{
+    UIStepper *stepper = sender;
     
+    NSInteger indexTag = stepper.tag;
+    double valueStepper = stepper.value;
+    
+    UILabel *scoreLabel = self.scoreLabels[indexTag];
+    scoreLabel.text = [NSString stringWithFormat:@"%d", (int)valueStepper];
+
     
 }
+
 
 @end
